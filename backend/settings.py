c0e4b4fcaca2ef29f347c5cc1763fde40f8e92c8
rikotse <11faith.c@gmail.com>
2025-08-12 00:56:23 +0200

    settings.py changes

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(gn^vj131p9-^8nig=$zo9wxiq1_2te)^@f!uwn&ex!rf73&p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['rikotse.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rikotse.pythonanywhere.com']
 
 
 # Application definition
