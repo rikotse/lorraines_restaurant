ff4143054ab2ccbd8ead3e1d99b9c97d46e34a5b
rikotse <11faith.c@gmail.com>
2025-08-11 23:50:12 +0200

    Ready for PythonAnywhere deploy

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1(gn^vj131p9-^8nig=$zo9wxiq1_2te)^@f!uwn&ex!rf73&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rikotse.pythonanywhere.com']
 
 
 # Application definition
