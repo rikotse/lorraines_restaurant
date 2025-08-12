ed3755250a1ad1969f3107f49b3c1569f368be72
Khwatsi Faith Chauke <11faith.c@gmail.com>
2025-08-12 15:24:13 +0000

    Save local changes before pulling

--- backend/settings.py
+++ backend/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-1(gn^vj131p9-^8nig=$zo9wxiq1_2te)^@f!uwn&ex!rf73&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['rikotse.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -40,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lorraines',
     'rest_framework',
-   
+
 ]
 
 MIDDLEWARE = [
@@ -118,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'lorraines/static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
