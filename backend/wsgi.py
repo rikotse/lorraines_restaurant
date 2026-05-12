785e0c1b07878fc2fc886d66001c25456e991667
rikotse <11faith.c@gmail.com>
2026-05-12 18:46:09 +0200

    feat: configure WSGI application with WhiteNoise for static file serving

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+app = application
